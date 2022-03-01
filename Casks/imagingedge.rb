1ce11037e2154877fa97a0571607f7b468b32cd2
Steven Dake <sdake@vmware.com>
2022-02-28 23:00:25 -0700

    Add sha256 for ied

--- Casks/imagingedge.rb
+++ Casks/imagingedge.rb
@@ -1,5 +1,6 @@
 cask "imagingedge" do
   version "1.1.00"
+  sha256 "12a78e224ff402a885e1e8c4d05805c67fef8570e91736c25816987ac30a3041"
   url "https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=en"
   name "Sony Imaging Edge"
   desc "Sony Imaging Edge"
