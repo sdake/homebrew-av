0f3efda8488a27bd15473c5647582d5fac82a857
Steven Dake <sdake@vmware.com>
2022-02-28 22:32:11 -0700

    Tidy up the decimator brew package

--- Casks/decimator.rb
+++ Casks/decimator.rb
@@ -1,9 +1,13 @@
-cask "decimator-control-panel" do
+cask "decimator" do
   version "3.0.0"
   sha256 "4b8a2cc909f89c734dcfea36dd8610cabc2d317f52c56c6a051e15974bbe93c3"
   url "http://decimator.com/specs/UCP%20#{version}.dmg"
   name "Decimator Control Panel"
-  desc "The Decimator 3.0.0 Control Panel"
+  desc "Control Decimator hardware via control panel"
   homepage "http://decimator.com/DOWNLOADS/DOWNLOADS.html"
-  app "Decimator Control Panel"
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+  app "UCP3.0.0"
 end
