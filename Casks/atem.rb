b65f54f89edb238a39d06f05312bfbceaf6d0e2b
Steven Dake <sdake@vmware.com>
2022-02-28 22:43:25 -0700

    update

--- Casks/atem.rb
+++ Casks/atem.rb
@@ -1,6 +1,6 @@
 cask "atem" do
   sha256 "700e858ea09b116ed94f4f67be386387d8d08288853a9324bfc8b248ca71d740"
-  url "https://sw.blackmagicdesign.com/ATEM/v8.6.4/Blackmagic_ATEM_Switchers_Macintosh_8.6.4.zip?Key-Pair-Id=APKAJTKA3ZJMJRQITVEA&Signature=hXPJr6mJ3x9Pby3OYi0/tw98DxN7c5Q+b72Mb5yW9cDKVrZD7QkIvRdp+xei011ohqqOL3AIzju36fLMkW0ev4oQos50ZKoSPoOew8gOixIlWcuD5JoFU2tWYcyNSpnEFjNuHqa9b+sCVYoY3PqC5xPCZLCm0awCv4swpUY12VY6yQ71xbIQQ2zUAJphKMfJdDtoNPuURMaDa2NTDKKoaeLzqglEtSyBjanRJDCTA80o9BA7p/CTh2AL4Aju2MVuUSVcvy50qOjaA2KBpMvHECyGAKJWzLlS7M+4TwthxCks+ClCd1jLIYoHoOT4Q0LWVKMrgo8sOQn+ruYDgGz7cg==&Expires=#{version}"
+  url "https://sw.blackmagicdesign.com/ATEM/v8.6.4/Blackmagic_ATEM_Switchers_Macintosh_8.6.4.zip?Key-Pair-Id=APKAJTKA3ZJMJRQITVEA&Signature=DN6J7d+lyzJippwtgNL6z3HTo0hqPqnO0e9BUDR8Hkn0b2Wd9yzfdnRSEkC10Zzz52BDWjTETHBDoVIm8JgazS9BJkT7+kmEXXWHe5FeejWNAq5m/WIQpETz83gMPVh2GnfY+N8muVpUS7m7jSOB5vGmMDdSGLhIMdmLrXEoJAh+nSLPthmZxWWzZ6xA52Qk2YXBSyqvPlIA6KmGXx2Duo8LAqWwV3VNr2ZV+zlhkl7NNSKVXHRBCQTX1+sUqXGvtczWO5IoUn+gdhm5zkdFSWm5xqCsrVTggfBInust1WjUrZnzKa6tPfZVHKR3ACT7waxZst6mXWLjk+SGz5dUXg==&Expires=1646124122"
   name "ATEM Software Control"
   desc "Control ATEM BlackMagic devices via their switching software."
   homepage "https://www.blackmagicdesign.com/support/"
