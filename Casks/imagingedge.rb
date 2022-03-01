5d84b48303dc499e5fa8cadebc0320fcaad91cb0
Steven Dake <sdake@vmware.com>
2022-02-28 22:59:42 -0700

    imaging edge

---
+++ Casks/imagingedge.rb
@@ -0,0 +1,7 @@
+cask "imagingedge" do
+  version "1.1.00"
+  url "https://support.d-imaging.sony.co.jp/disoft_DL/desktop_DL/mac?fm=en"
+  name "Sony Imaging Edge"
+  desc "Sony Imaging Edge"
+  homepage "https://imagingedge.sony.net/en-us/ie-desktop.html"
+end
