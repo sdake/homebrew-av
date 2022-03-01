73f29e8a72a2be8e9c4c06c8b4f71dc8008a3a90
Steven Dake <sdake@vmware.com>
2022-02-28 22:07:04 -0700

    Decimator USB control panel

---
+++ Casks/decimator-control-panel.rb
@@ -0,0 +1,9 @@
+cask "decimator-control-panel" do
+  version "3.0.0"
+  sha256 "4b8a2cc909f89c734dcfea36dd8610cabc2d317f52c56c6a051e15974bbe93c3"
+  url "http://decimator.com/specs/UCP%20#{version}.dmg"
+  name "Decimator Control Panel"
+  desc "The Decimator 3.0.0 Control Panel"
+  homepage "http://decimator.com/DOWNLOADS/DOWNLOADS.html"
+  app "Decimator Control Panel"
+end
